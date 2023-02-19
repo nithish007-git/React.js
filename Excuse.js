a8efe5b624eed16dc5d9ab5fcb2a72ae3204bdcd
nithish <63574713+nithish007-git@users.noreply.github.com>
2023-02-19 23:06:35 +0530

    Update Excuse.js

--- Excuse.js
+++ Excuse.js
@@ -43,3 +43,40 @@ function App(){
 
 }
 export default App;
+
+
+
+
+//version--2
+import "./App.css";
+import {useEffect,useState} from "react";
+import Axios from "axios";
+
+function App(){
+  const [party,setparty]=useState("");
+  // const[Family,setFamily]=useState("");
+  //  const[Office,setOffice]=useState("");
+
+  const party1=(excuse)=>{
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res)=>{
+      setparty(res.data[0].excuse);
+
+    }
+    );
+  };
+
+
+  return(
+    <div className="App">
+      <h1>Genrate An Excuse</h1>
+      <button onClick={() =>party1("party")}>Party</button>
+      <button onClick={() =>party1("office")}>office</button>
+      <button onClick={() =>party1("family")}>family</button>
+
+<p>{party}  </p>
+    </div>
+  )
+
+}
+export default App;
+
